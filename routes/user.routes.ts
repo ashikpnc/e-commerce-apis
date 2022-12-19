06ae971342fa61a408ae4b2a9bcc4d14f2dcdb4d
ashik <ashikpnc@gmail.com>
2022-12-19 19:18:17 +0530

    structure updates

--- routes/user.routes.ts
+++
@@ -1,9 +0,0 @@
-import express from 'express'
-import { deleteProfile, getProfile, updateProfile } from '../controllers/user.controllers'
-
-const router = express.Router()
-router.route('/profile')
-    .get(getProfile)
-    .patch(updateProfile)
-    .delete(deleteProfile)
-export default router
